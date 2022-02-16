4f8843ed3b82f9bfab3fbbb9cc2a1d78585e2c55
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2022-02-16 13:51:06 +0800

    add more sample

--- app.vue
+++ app.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
+    <n-result status="success" title="succss" description="naive-ui now perfectly supports nuxt3">
+      <template #footer>
+        <n-button type="success">Button</n-button>
+      </template>
+    </n-result>
     <n-space>
       <n-button type="primary">naive-ui</n-button>
       <n-button type="info">naive-ui</n-button>
       <n-button type="warning">naive-ui</n-button>
+    </n-space>
+    <n-h2>Tooltip demo</n-h2>
+    <n-tooltip trigger="hover">
+      <template #trigger>
+        <n-button>鸭子</n-button>
+      </template>
+      如果它长得像鸭子，走起来像鸭子，叫起来也像鸭子，那它一定是个鸭子。
+    </n-tooltip>
+    <n-h1>特点</n-h1>
+    <n-space>
+      <n-tag>Nuxt3</n-tag>
+      <n-tag>Vite</n-tag>
+      <n-tag>Naive-ui</n-tag>
     </n-space>
   </div>
 </template>
